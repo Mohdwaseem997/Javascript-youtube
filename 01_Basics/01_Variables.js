b18ad3876a83a55b6a4823a7ff3c35a0d8585849
Mohdwaseem997 <mohdwaseemullah1999@gmail.com>
2026-09-06 23:23:54 +0000

    Update variable examples

--- 01_Basics/01_Variables.js
+++ 01_Basics/01_Variables.js
@@ -2,6 +2,7 @@ const accountId = 123456
 let accountEmail = "user@example.com"
 var accountPassword = "password123"
 accountCity = "New York"
+let accountState;
 
 // accountId = 654321 // changes like this will throw an error because accountId is a constant
 
@@ -9,8 +10,9 @@ accountCity = "New York"
  accountPassword = "newpassword456"
  accountCity = "Los Angeles"
 
- /*
- perfer not to use var because it is function scoped and can lead to unexpected behavior.
- */
+//  perfer not to use var because it is function scoped and can lead to unexpected behavior.
+//  We should use let and const instead because they are block scoped and provide better control over variable scope.
+//  variables without a unassigned value will be undefined by default. For example, accountState is declared but not assigned a value, so it will be undefined.
+ 
 
-console.table([accountId, accountEmail, accountPassword, accountCity])
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
